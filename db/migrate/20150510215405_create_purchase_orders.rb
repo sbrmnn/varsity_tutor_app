148b951e85bbf83e09c10ee8b09e3de7f23fd549
gth824c@gmail.com <arunsubramanian@Arun-Subramanians-MacBook-Pro.local>
2015-05-11 01:09:14 -0400

    Got rid of more product stuff

--- db/migrate/20150510215405_create_purchase_orders.rb
+++ db/migrate/20150510215405_create_purchase_orders.rb
@@ -1,7 +1,6 @@
 class CreatePurchaseOrders < ActiveRecord::Migration
   def change
     create_table :purchase_orders do |t|
-      t.belongs_to :product, null: false
       t.string :purchasers_name, null: false
       t.string :address, null: false
       t.string :city, null: false
@@ -9,7 +8,6 @@ class CreatePurchaseOrders < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    add_index(:purchase_orders, :product_id)
     add_index(:purchase_orders, :purchasers_name)
     add_index(:purchase_orders, [:address, :city, :state])
   end
