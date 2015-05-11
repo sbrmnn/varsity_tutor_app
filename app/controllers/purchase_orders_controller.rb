f64dcca950830ab184a8abdbd0deffa924c4a972
gth824c@gmail.com <arunsubramanian@Arun-Subramanians-MacBook-Pro.local>
2015-05-11 13:12:12 -0400

    Added controller code that passes tests

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,2 +1,24 @@
 class PurchaseOrdersController < ApplicationController
+  def new
+    @purchase_order = PurchaseOrder.new
+  end
+
+  def create
+    @purchase_order = PurchaseOrder.new(purchase_order_params)
+    if @purchase_order.save
+      redirect_to @purchase_order
+    else
+      render :new
+    end
+  end
+
+  def show
+    @purchase_order = PurchaseOrder.find(params[:id])
+  end
+
+  private
+
+  def purchase_order_params
+    params.require(:purchase_order).permit(:purchasers_name, :address, :city, :state)
+  end
 end
