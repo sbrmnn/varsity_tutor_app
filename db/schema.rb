5e6bfb2f3af90e15f18a4b6438b196080dac135d
gth824c@gmail.com <arunsubramanian@Arun-Subramanians-MacBook-Pro.local>
2015-05-11 13:13:25 -0400

    Did migration

--- db/schema.rb
+++ db/schema.rb
@@ -13,27 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20150510215405) do
 
-  create_table "products", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "photo_url",  null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "products", ["name"], name: "index_products_on_name"
-
   create_table "purchase_orders", force: :cascade do |t|
-    t.integer  "product_id",      null: false
-    t.string   "purchasers_name", null: false
-    t.string   "address",         null: false
-    t.string   "city",            null: false
-    t.string   "state",           null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "purchasers_name", limit: 255, null: false
+    t.string   "address",         limit: 255, null: false
+    t.string   "city",            limit: 255, null: false
+    t.string   "state",           limit: 255, null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
-  add_index "purchase_orders", ["address", "city", "state"], name: "index_purchase_orders_on_address_and_city_and_state"
-  add_index "purchase_orders", ["product_id"], name: "index_purchase_orders_on_product_id"
-  add_index "purchase_orders", ["purchasers_name"], name: "index_purchase_orders_on_purchasers_name"
+  add_index "purchase_orders", ["address", "city", "state"], name: "index_purchase_orders_on_address_and_city_and_state", using: :btree
+  add_index "purchase_orders", ["purchasers_name"], name: "index_purchase_orders_on_purchasers_name", using: :btree
 
 end
