c8617053f1610dc5c920574e23c21bcc98e03c90
gth824c@gmail.com <arunsubramanian@Arun-Subramanians-MacBook-Pro.local>
2015-05-11 00:39:05 -0400

    Got rid of products factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,4 @@
 FactoryGirl.define do
-
-  factory :product do
-    name "wunder-beverage"
-    photo_url  "http://library.the-group.net/rexam/client_upload/image/Grolsch_3FM_Serious_Request.jpg"
-  end
-
   factory :purchase_order do
     product
     purchasers_name "First Name and Last Name"
