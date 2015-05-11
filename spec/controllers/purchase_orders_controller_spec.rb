b9dc281c416da20f90e17109aa8c58ab4843e752
gth824c@gmail.com <arunsubramanian@Arun-Subramanians-MacBook-Pro.local>
2015-05-11 01:14:06 -0400

    Added some controller specs

--- spec/controllers/purchase_orders_controller_spec.rb
+++ spec/controllers/purchase_orders_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseOrdersController, type: :controller do
+  describe "GET index" do
+    it "assigns @purchase_order" do
+      get :new
+      expect(assigns(:purchase_order).persisted?).to eq(false)
+    end
 
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST create" do
+    it "should create purchase order" do
+      post :create, :purchase_order => build(purchase_order)
+      expect(assigns(:purchase_order).persisted?).to eq(true)
+      expect(response).to render_template(:show)
+    end
+
+  end
 end
