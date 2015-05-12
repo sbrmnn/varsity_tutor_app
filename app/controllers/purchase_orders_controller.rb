8dad22a16cb9a08ff070d2c9df04e6f38abc8c3f
gth824c@gmail.com <arunsubramanian@Arun-Subramanians-MacBook-Pro.local>
2015-05-12 16:07:39 -0400

    Limited routes to new and create

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,22 +1,29 @@
 class PurchaseOrdersController < ApplicationController
   def new
     @purchase_order = PurchaseOrder.new
+    purchase_order_all
   end
 
   def create
     @purchase_order = PurchaseOrder.new(purchase_order_params)
     if @purchase_order.save
-      redirect_to @purchase_order
+      redirect_to :root
     else
+      flash[:error] = @purchase_order.errors
+      purchase_order_all
       render :new
     end
   end
 
-  def show
-    @purchase_order = PurchaseOrder.find(params[:id])
-  end
-
   private
+
+  def purchase_order_all
+    @purchase_order_all = []
+    PurchaseOrder.find_each do |po|
+      @purchase_order_all.unshift(po)
+    end
+    @purchase_order_all.flatten!
+  end
 
   def purchase_order_params
     params.require(:purchase_order).permit(:purchasers_name, :address, :city, :state)
