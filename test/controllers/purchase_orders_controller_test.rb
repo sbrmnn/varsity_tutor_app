3afae5b98492f5d8caed8127efd4def8f4f1d986
gth824c@gmail.com <arunsubramanian@Arun-Subramanians-MacBook-Pro.local>
2015-05-13 01:18:36 -0400

    Added test code to limit amount of record from all query

--- test/controllers/purchase_orders_controller_test.rb
+++ test/controllers/purchase_orders_controller_test.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../test_helper", __FILE__)
 class PurchaseOrdersControllerTest < ActionController::TestCase
   def setup
     @purchase_order = create(:purchase_order)
+    @purchase_order_all_desc_limit_20 = PurchaseOrder.order("id DESC").all.limit(20)
   end
-
   def test_new_response
     get :new
     assert_response :success
@@ -13,10 +13,9 @@ class PurchaseOrdersControllerTest < ActionController::TestCase
     get :new
     assert_equal(false, assigns(:purchase_order).persisted?)
   end
-  def test_new_object_purchase_order_all
-
+  def test_new_object_purchase_order_all_desc_limit_20
     get :new
-    assert_equal(PurchaseOrder.all, assigns(:purchase_order_all))
+    assert_equal(@purchase_order_all_desc_limit_20, assigns(:purchase_order_all))
   end
   def test_create_action
     post :create, purchase_order: attributes_for(:purchase_order)
@@ -27,31 +26,31 @@ class PurchaseOrdersControllerTest < ActionController::TestCase
   def test_create_purchase_order_with_all_incomplete_fields
     post :create, purchase_order: {purchasers_name: nil, address: nil, city: nil, state: nil }
     assert_equal( false, assigns(:purchase_order).persisted?)
-    assert_equal(PurchaseOrder.all, assigns(:purchase_order_all))
+    assert_equal(@purchase_order_all_desc_limit_20, assigns(:purchase_order_all))
     assert_template("new")
   end
   def test_create_purchase_order_with_address_incomplete_field
     post :create, purchase_order: {purchasers_name: nil, address: "30 5th st", city: "Atlanta", state: "Ga" }
     assert_equal( false, assigns(:purchase_order).persisted?)
-    assert_equal(PurchaseOrder.all, assigns(:purchase_order_all))
+    assert_equal(@purchase_order_all_desc_limit_20, assigns(:purchase_order_all))
     assert_template("new")
   end
   def test_create_purchase_order_with_address_incomplete_field
     post :create, purchase_order: {purchasers_name: "Arun Subramanian", address: nil, city: "Atlanta", state: "Ga" }
     assert_equal( false, assigns(:purchase_order).persisted?)
-    assert_equal(PurchaseOrder.all, assigns(:purchase_order_all))
+    assert_equal(@purchase_order_all_desc_limit_20, assigns(:purchase_order_all))
     assert_template("new")
   end
   def test_create_purchase_order_with_city_incomplete_field
     post :create, purchase_order: {purchasers_name: "Arun Subramanian", address: "30 5th st", city: nil, state: "Ga" }
     assert_equal( false, assigns(:purchase_order).persisted?)
-    assert_equal(PurchaseOrder.all, assigns(:purchase_order_all))
+    assert_equal(@purchase_order_all_desc_limit_20, assigns(:purchase_order_all))
     assert_template("new")
   end
   def test_create_purchase_order_with_state_incomplete_field
     post :create, purchase_order: {purchasers_name: "Arun Subramanian", address: "30 5th st", city: "Atlanta", state: nil }
     assert_equal( false, assigns(:purchase_order).persisted?)
-    assert_equal(PurchaseOrder.all, assigns(:purchase_order_all))
+    assert_equal(@purchase_order_all_desc_limit_20, assigns(:purchase_order_all))
     assert_template("new")
   end
 end
